044e7bc0dd739c17a5bd04892fcdfa21d637661c
Kapil Gurnani <kapilgurnani@DEL-LG73JL9P4P.local>
2025-06-20 17:01:15 +0530

    updated app name

--- assets/images/index.js
+++ assets/images/index.js
@@ -2,7 +2,8 @@ import logo from './logo.png';
 import team1 from './team-1.jpg';
 import team2 from './team-1.jpg';
 import team3 from './team-1.jpg';
+import { curved9 } from './curved-images';
 
 export {
-    logo, team1, team2, team3
+    logo, team1, team2, team3, curved9
 }
